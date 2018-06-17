b34432c18f02cf728dfbf9ba414fce35dd34a37d
DobrinGanev <dobringanev@gmail.com>
2018-06-17 17:50:41 -0600

    added react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-
-const dev = process.env.NODE_ENV !== 'production';
-
-const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-    template: path.join(__dirname, '/src/index.html'),
-    filename: 'index.html',
-    inject: 'body',
-});
-
-const DefinePluginConfig = new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-});
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    devServer: {
-        host: 'localhost',
-        port: '3000',
-        hot: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-        historyApiFallback: true,
+    entry: ['./src/index'],
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    entry: ['react-hot-loader/patch', path.join(__dirname, '/src/index.jsx')],
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['babel-loader'],
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader',
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                },
+                exclude: /node_modules|packages/,
+                test: /\.js$/,
+                use: 'babel-loader',
             },
         ],
     },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.join(__dirname, '/build'),
-    },
-    mode: dev ? 'development' : 'production',
-    plugins: dev
-        ? [
-            HTMLWebpackPluginConfig,
-            new webpack.HotModuleReplacementPlugin(),
-        ]
-        : [HTMLWebpackPluginConfig, DefinePluginConfig],
-};
+    plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
+}
